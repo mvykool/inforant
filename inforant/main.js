7baaadf35c7ca9bea18ead6954746f187120a8de
maicol hernandez <maykolandres4@gmail.com>
2022-09-11 19:43:59 -0500

    added navigation

--- inforant/main.js
+++ inforant/main.js
@@ -24,23 +24,6 @@ closeMenu.addEventListener("click", () => {
     burgerMenu.style.display = "block";
 });
 
-//call to api to get background
-
-async function getBackground() {
-    const results = await API("playercards/2d91369b-4fb5-28c6-3111-c285c9a32c2c");
-    const data = results.data;
-    console.log(data)
-
-    const container = document.querySelector(".background");
-    const img = document.createElement("img");
-    img.setAttribute("src", data.data.largeArt);
-    img.classList.add("img-bg-size");
-    container.appendChild(img);
-
-
-}
-
-getBackground()
 
 //call api for previews
 
@@ -59,13 +42,13 @@ async function getAgentPreview() {
     });
 }
 
-getAgentPreview()
 
 //get maps preview
 
 async function getMapPreview() {
     const results = await API("maps");
     const data = results.data.data.slice(0, 3);
+
 
     console.log(data)
 
@@ -78,7 +61,6 @@ async function getMapPreview() {
     });
 }
 
-getMapPreview()
 
 //get cards preview
 
@@ -97,7 +79,6 @@ async function getCardPreview() {
     });
 }
 
-getCardPreview()
 
 //get weapons preview
 
@@ -116,4 +97,30 @@ async function getWeaponPreview() {
     });
 }
 
-getWeaponPreview()
+
+
+window.addEventListener("DOMContentLoaded", navigator, false);
+window.addEventListener("hashchanged", navigator, false);
+
+
+function navigator() {
+    if (location.hash.startsWith("#agents")) {
+        filmPage()
+    } else if (location.hash.startsWith("#maps")) {
+        moviePage()
+    } else if (location.hash.startsWith("#cards")) {
+        moviePage()
+    } else if (location.hash.startsWith("#weapons")) {
+        moviePage()
+    } else {
+        homePage()
+    }
+}
+
+function homePage() {
+    getWeaponPreview()
+    getCardPreview()
+    getMapPreview()
+    getAgentPreview()
+
+}
